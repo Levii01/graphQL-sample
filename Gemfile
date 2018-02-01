bf7d274b4d5e4419adc2c3b1bfc658f3a8a62a26
Levii01 <m.sedziak01@gmail.com>
2018-02-01 21:58:54 +0100

    Add seeds user with projects

--- Gemfile
+++ Gemfile
@@ -33,11 +33,12 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'devise', '~> 4.2'
-gem 'ffaker', '~> 2.8', '>= 2.8.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'ffaker', '~> 2.8', '>= 2.8.1'
+  gem 'pry'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
 PROJECT_TITLES = 10.times.map { FFaker::Internet.domain_word }
+puts PROJECT_TITLES
+
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.size * SPLIT
+
+PROJECT_USERS = {
+  'Fiz@test.com' => PROJECT_TITLES[0, OFFSET],
+  'Baz@test.com' => PROJECT_TITLES[OFFSET..-1]
+}.freeze
+
+PROJECT_USERS.each do |email, projects|
+  user = User.new(email: email, password: 'secret')
+  projects.each { |title| user.projects.build(title: title) }
+  user.save
+end
