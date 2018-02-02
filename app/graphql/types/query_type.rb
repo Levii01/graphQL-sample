6780a3bd3aebcbe05c773718d8a59d1a2c360cab
Levii01 <m.sedziak01@gmail.com>
2018-02-02 09:30:13 +0100

    Add basic graphQL schema

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,27 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
 
-  # TODO: remove me
   field :testField, types.String do
     description "An example field added by the generator"
     resolve ->(obj, args, ctx) {
       "Hello World!"
     }
+  end
+
+  field :me, Types::UserType do
+    description "the current user"
+    resolve ->(obj, args, ctx) {
+      User.first
+    }
+  end
+
+  field :projects, types[Types::ProjectType] do
+    description "the projects for the current user"
+    resolve ->(obj, args, ctx) { Project.all }
+  end
+
+  field :my_projects, types[Types::ProjectType] do
+    description "the projects for the current user"
+    resolve ->(obj, args, ctx) { User.first.projects }
   end
 end
